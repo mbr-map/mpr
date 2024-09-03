1e7bf18e0726db551c399f047c765ba9406865fc
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-03 07:23:16 +0000

    newly-added files

---
+++ example3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Write a Shell script  to check the file existences or not ? file existence means display the file-content. or else create the file with content..
+
+echo " Check weather the file existences or not..."
+read -p " Please enter the filename :" filename
+if [[ -f $filename ]]
+then
+	echo " $filename this file is existence... "
+	echo " displaying the file content....."
+	cat $filename
+else
+	echo " $filename this file is not existence.."
+	echo " Creating the file and updating the content.."
+	cat > $filename
+fi
---
+++ example4.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# write a shell script to Check weather the empty dir existance or not
+echo " Check weather the dir existence or not "
+read -p " Please enter the directory-name :" dirname
+if [[ ! -r $dirname ]]
+then
+echo " $dirname is not existence..."
+else
+echo " $dir is existence..."
+fi
