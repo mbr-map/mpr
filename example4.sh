e10116f6ce6ce9f1c0b631805ba508b92c7af99d
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-03 07:39:25 +0000

    Update this files

--- example4.sh
+++ example4.sh
@@ -2,9 +2,10 @@
 # write a shell script to Check weather the empty dir existance or not
 echo " Check weather the dir existence or not "
 read -p " Please enter the directory-name :" dirname
-if [[ ! -r $dirname ]]
+if [[ -r $dirname ]]
 then
 echo " $dirname is not existence..."
 else
 echo " $dir is existence..."
 fi
+echo " updated this file........"
