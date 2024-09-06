4683c2a7d75456d71c8f4988ddf1d04c38b0bf4f
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-06 00:21:44 +0000

    new file updated

--- example5.sh
+++ example5.sh
@@ -1,1 +1,10 @@
 echo " This code is updated from file..........."
+# write a shell script to write the 1 to 100 in the for-loop "
+
+echo " for-loops started........."
+
+for((a=1;a<=100;a++))
+do
+	echo " a value is : $a"
+done
+
