8ad6ba96bb9eb2c5b81d28f7368601945e9544bf
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-09 11:27:58 +0000

    taskno:2 taskname:ex8

--- example8.sh
+++ example8.sh
@@ -6,4 +6,15 @@ add()
 	a=10
 	b=20
 	sum=`expr $a + $b `
-	echo " Adding the two numbers $sum
+	echo " Adding the two numbers $sum"
+}
+sub()
+{
+	a=60
+	b=10
+	sub=`expr $a - $b`
+	echo "subtracting the two numbers $sub"
+
+}
+add
+sub
