a771778d3d684c5a2f75645dc5b55497b764cd91
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-09 07:35:14 +0000

    Task no:1 task name : ex3

--- example3.sh
+++ example3.sh
@@ -11,30 +11,7 @@ then
 else
 	echo " $filename this file is not existence.."
 	echo " Creating the file and updating the content.."
-	cat > $filename
+	cat > $filenam
 fi
+echo " some code removed from exam3 from dev1 branch.."
 
-# updating the new features to check the directories
-
-echo " Check weathe the director existence or not? existence means open the that dir"
-read -p " Please enter the directory name :" dirname
-if [[ -r $dirname ]] 
-then
-
-	echo " $dirname is existences.."
-	ls /$dirname
-else
-	echo " $dirname not existence"
-fi
-
-echo " updating the new features..."
-#write a shell script to check the directory existence or not?
-
-echo " Check weather the dir existence or not "
-read -p " Please enter the dirname: " dir
-if [[ -r $dir ]]
-then
-	echo " $dir is existence"
-else
-	echo " $dir is not existence"
-fi
