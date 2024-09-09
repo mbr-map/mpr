f3194c3a544948c6ee06526ff3a693c18d7e0797
Bharathmbr <bharathreddymbrr@gmail.com>
2024-09-09 06:36:20 +0000

    task no : 1 task name : ex6

---
+++ example6.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "write a shell script to find the numbers from 1 to 10 using while-loop and also in the for loop "
+
+a=1
+while (( $a <=  10 ))
+do
+	echo "while-loop :$a"
+	(( a++ ))
+done
