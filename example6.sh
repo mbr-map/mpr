2351e0c6fd756287615e721cc6845f941e3f0d36
bharathmbr <113525016+bharathmbr@users.noreply.github.com>
2024-09-09 17:09:58 +0530

    Update example6.sh
    updated new code

--- example6.sh
+++ example6.sh
@@ -11,3 +11,10 @@ done
 echo " the code is update"
 
 echo " new feature is update.."
+
+echo "  iterating from 1 to 1oo using the for loop"
+echo " for-loop startting.........."
+for (( a=1; a<=100; a++ ))
+do
+echo $a
+done
