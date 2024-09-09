b862f9def93f1b1d25aa90fd52f68ad6ad4936d4
bharathmbr <113525016+bharathmbr@users.noreply.github.com>
2024-09-09 13:54:06 +0530

    Update example7.sh
    updated new features

--- example7.sh
+++ example7.sh
@@ -4,4 +4,17 @@ a=10
 b=20
 sum=`expr $a + $b`
 echo "Adding two numbers.. $sum"
-
+echo " -----------------------------------------------------------------------"
+echo " write a shell script by Arithmematic operations....."
+a=10
+b=20
+c=30
+echo " Adding the three numbers.."
+sum=`expr $a + $b + $c`
+echo "Adding the three numbers is $sum"
+echo " subtrscting the three numbers"
+sub=`expr $a - $b - $c`
+echo " subtracting the numbers are $sub"
+echo " Multiplication of three numbers.."
+mul=`expr $a \* $b \*  $c`
+echo " Multiplying the three numbers  is $mul"
